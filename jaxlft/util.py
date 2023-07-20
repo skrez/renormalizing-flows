aa00b05a9256b097c19e8eac3f190ccb63e8cb5c
skrez <semonr@gmail.com>
2023-07-20 18:27:24 -0400

    rescaled_norms typo

--- jaxlft/util.py
+++ jaxlft/util.py
@@ -358,7 +358,7 @@ class CarossoPrior:
         phips = our_fft(phis) 
         norms = jax.lax.real(phips*jax.lax.conj(phips))
         rescaled_norms = -(hatpsquared/self.Omega) *  norms
-        uncorrected_sums = jnp.sum(rescaled, axis=(-1,-2))/2
+        uncorrected_sums = jnp.sum(rescaled_norms, axis=(-1,-2))/2
         return uncorrected_sums + (rescaled_norms[:, 0, 0]/2) + (rescaled_norms[:, int(self.N)/2, int(self.N)/2]/2)
 
 
